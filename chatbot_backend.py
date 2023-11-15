8adaa3f12c4a9821624b94e4737e2bdcd27257e3
Daniel Lapizco Pérez <50065724+daniel-lapizco-perez@users.noreply.github.com>
2023-11-15 17:56:45 -0600

    Cambios al chatbot, diccionario con valores importantes

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -1,21 +1,33 @@
 import os
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 def chatbot(): 
 
+    ticket = {"description": [], "area": []}
     messages = [
         {"role": "system", "content": "Crea tus tickets de manera rápida con IA"},
     ]
 
-    while True:
-        message = input("Usuario: ")
+    input("Usuario: ")
+    input_description = input("¿Cuál es el problema que está presentando?: ")
+    ticket["description"].append(input_description)
+    print("1. Hardware \n2. Software \n3. Periféricos")
+    input_area = input("Seleccione una área especifica donde esté pasando su problema de la lista de arriba: ")
+    ticket["area"].append(input_area)
+    #print(ticket)
+    print("¿Qué desea hacer con la información previamente proporcioanda? \nAbrir un ticket o continuar")
 
+    while True:
+        
+        message =  input("Usuario: ")
         if message.lower() == "quit":
             break
 
         messages.append({"role": "user", "content": message})
+
+        
 
         response = openai.chat.completions.create(
             model="gpt-3.5-turbo",
