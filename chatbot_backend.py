5463ba15fffb46ebf11811eb3da0b9fd68c73810
Daniel Lapizco Pérez <50065724+daniel-lapizco-perez@users.noreply.github.com>
2023-11-16 05:01:38 -0600

    ruta de post de tickets en chatbot

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -1,29 +1,45 @@
 import os
 import openai
 from datetime import datetime
+from flask import Flask
+from flask_socketio import SocketIO
+import requests
+import json
 
-openai.api_key = "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"
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+openai.api_key = "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"
+messages = [
+    {"role": "system", "content": "Crea tus tickets de manera rápida con IA"},
+]
+
+@app.route("/")
+def index():
+    return "200"
 
 def chatbot(): 
-
-    ticket = {"description": [], "area": [], "creation": []}
-    messages = [
-        {"role": "system", "content": "Crea tus tickets de manera rápida con IA"},
-    ]
+    ticket = {"title": [], "description": [], "author": [""], "assigned":[""], "area": [], "creation": [], "modified": [], "status": ['Abierto']}
+   
     #mensaje inicial
-    input("Usuario: ")
+    input_title = input("Titulo de ticket: ")
+    ticket["title"].append(input_title)
+
     #Input de descripcion
     input_description = input("¿Cuál es el problema que está presentando?: ")
     ticket["description"].append(input_description)
+
     #Input de tipos
     print("1. Hardware \n2. Software \n3. Periféricos \n4. Solicitud de equipo(periféricos)")
     input_area = input("Seleccione una área especifica donde esté pasando su problema de la lista de arriba: ")
     ticket["area"].append(input_area)
+
     #obtener fecha y hora actual
-    time = datetime.now()
-    moded_time = time.strftime("%Y-%m-%d %H:%M:%S")
+    moded_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     ticket["creation"].append(str(moded_time))
-    #print(ticket)
+    ticket["modified"].append(str(moded_time))
+    print(ticket)
 
 
     print("¿Qué desea hacer con la información previamente proporcioanda? \nAbrir un ticket o continuar")
@@ -44,8 +60,36 @@ def chatbot():
         chat_message = response.choices[0].message.content
         print(f"Help Desk ChatBot: {chat_message}")
         messages.append({"role": "assistant", "content": chat_message})
+
+    post_ticket(ticket)
     
     return ticket
+
+def post_ticket(ticket):
+    url = "http://172.174.216.130:5000/ticket_post"
+
+    headers = {'Content-Type': 'application/json'}
+
+    moded_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    data = {
+        "title" : ticket['title'][0],
+        "description": ticket['description'][0],
+        "area": ticket['area'][0],
+        "status": ticket['status'][0],
+        "percentage": "0",
+        "created": moded_time,
+        "modified": moded_time,
+        "author": ticket['author'][0],
+        "assigned": ticket['assigned'][0]
+    }
+
+    try:
+        response = requests.post(url, json=data, headers=headers)
+        response.raise_for_status()
+        print(f'Solicitud exitosa: {response.text}')
+    except requests.exceptions.RequestException as e:
+        print('Error: ', e)
 
 if __name__ == "__main__":
     print ("Crea un ticket con IA")
